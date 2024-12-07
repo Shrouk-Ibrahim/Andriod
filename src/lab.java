9af104200634c3fee6a03dbcd2c517d38e600458
shroukIbrahim646 <shroukkhan52@gmail.com>
2024-12-07 15:42:16 +0200

    Add project files

---
+++ src/lab.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class lab {
+    public static void main(String[] args) {
+        // Create an ArrayList to store random integers
+        ArrayList<Integer> randomNumbers = new ArrayList<>();
+        Random random = new Random();
+//val multibythree :(Int) ->(Int) ={number -> number*2}
+//val multibythreeee={number :Int -> number*2}
+//val multibythreee :(Int) ->(Int) ={it*2}
+//val info :(String,Int) -> (Unit) ={name ,age->
+//    println("info is $name and $age ")
+//}
+//info("Df",3)
+//    println(multibythree(8))
+//    println(multibythreee(8))
+//    println(multibythreeee(8))
+//    fun encodeMsg(msg: String, encode: (String) -> String): String {
+//        return encode(msg)
+//    }
+//    val enc1: (String) -> String = { input -> input.toUpperCase() }
+//    fun enc2(input:String): String = input.reversed()
+//    println(encodeMsg("abc", enc1))
+//   println(encodeMsg("abc", ::enc2))
+//    val list=List(100){(0..100).random()}
+//    val mylist= mutableListOf<Int>()
+//    for (i in 1..100) {
+//        mylist.add(i)
+//    }
+//    println(mylist)
+//    println(list)
+//    val books = listOf("nature", "biology","birds")
+//   // println(books.filter { it[0] == 'b' })
+//    println(books.filter { c->c[0] == 'b' })
+//    val str = listOf("shrouk","youssef","habiba")
+//    val mapped=str.map { it.contains('s') }
+//    val zipped =str.zip(mapped)
+//    println(zipped)
+//        val nestedlist= listOf(
+//                setOf(3,34,2),
+//                (0..6).toList()
+//    )
+//        println(nestedlist)
+    }}
